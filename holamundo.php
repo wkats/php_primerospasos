5f9c0e8038b2528ebdc48ab07a5e4666c8322894
wkats <wkatsmail@gmail.com>
2016-12-19 22:05:02 -0600

    Hola Mundo Agregado :)

---
+++ holamundo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>:.Hola Mundo PHP.:</title>
+</head>
+<body>
+
+<?php
+echo "<table>";
+echo "	<tr>";
+echo "		<th>¡Hola </th>";
+echo "		<th> Mundo!</th>";
+echo "	</tr>";
+echo "	<tr>";
+echo "		<td>Contenido</td>";
+echo "		<td> programado ;)</td>";
+echo "	</tr>	";
+echo "</table>";
+?>
+	
+</body
+></html>
